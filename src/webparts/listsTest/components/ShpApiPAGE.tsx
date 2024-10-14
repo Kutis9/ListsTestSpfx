fd33144265047c2b0a2addc944e834936b5dcec4
Kutis9 <lukas.kutka@hotmail.com>
2024-10-14 14:15:13 +0200

    final CRUD operacie

--- src/webparts/listsTest/components/ShpApiPAGE.tsx
+++ src/webparts/listsTest/components/ShpApiPAGE.tsx
@@ -1,6 +1,7 @@
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { SPHttpClient } from '@microsoft/sp-http';
 import * as React from 'react';
+import styles from './ShpApiPAGE.module.scss';
 
 export interface IShpApiPAGEProps {
     context: WebPartContext;
@@ -12,6 +13,9 @@ export interface IShpApiPAGEState {
     meno: string;
     priezvisko: string;
     deletePriezvisko: string;
+    updateId: string;
+    updateMeno: string;
+    updatePriezvisko: string;
 }
 
 export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpApiPAGEState> {
@@ -23,7 +27,10 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
             items: [],
             meno: '',
             priezvisko: '',
-            deletePriezvisko: ''
+            deletePriezvisko: '',
+            updateId: '',
+            updateMeno: '',
+            updatePriezvisko: ''
         };
     }
     handleMenoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +42,18 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
     handlePriezviskoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ priezvisko: event.target.value });
     }
+    
+    handleUpdateIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ updateId: event.target.value });
+    }
+
+    handleUpdateMenoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ updateMeno: event.target.value });
+      }
+    
+      handleUpdatePriezviskoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ updatePriezvisko: event.target.value });
+      }
 
     handleDeletePriezviskoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ deletePriezvisko: event.target.value });
@@ -44,6 +63,7 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
         this.setState({ listName: event.target.value });
     }
 
+    // CREATE ITEM
     handleCreateClick = () => {
         const item = {
             __metadata: {
@@ -67,6 +87,7 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
         })
         .then(response => response.json())
         .then(data => {
+            alert('Položka bola úspešne vytvorená');
             console.log(data);            
             this.setState({ meno: '', priezvisko: '' });
             this.handleLoadClick();
@@ -76,6 +97,7 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
         });
     }
 
+    // DELETE ITEM
     handleDeleteClick = () => {
 
 
@@ -95,9 +117,9 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
                 })
                 .then(() => {
                     console.log(data);
+                    alert(`Polozka s priezviskom ${this.state.deletePriezvisko} vymazana`);
                     this.setState({ deletePriezvisko: '' });
                     this.handleLoadClick();
-                    alert(`Polozka s priezviskom ${this.state.deletePriezvisko} vymazana`);
                 })
                 .catch(error => {
                     console.error(error);
@@ -113,6 +135,39 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
         });
     }
 
+    // UPDATE ITEM
+    handleUpdateClick = () => {
+        const { context } = this.props;
+        const { updateId, updateMeno, updatePriezvisko } = this.state;
+    
+        const item = {
+          __metadata: {
+            type: 'SP.Data.CRUDLISTListItem'
+          },
+          Meno: updateMeno,
+          Priezvisko: updatePriezvisko
+        };
+    
+        context.spHttpClient.post(`${context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('CRUDLIST')/items(${updateId})`, SPHttpClient.configurations.v1, {
+          headers: {
+            'Accept': 'application/json;odata=verbose',
+            'Content-type': 'application/json;odata=verbose',
+            'odata-version': '',
+            'X-HTTP-Method': 'MERGE',
+            'If-Match': '*'
+          },
+          body: JSON.stringify(item)
+        })
+          .then(() => {
+            console.log('Položka bola úspešne aktualizovaná');
+            alert(`Položka s ID: ${updateId} bola úspešne aktualizovaná`);
+            this.setState({ updateId: '', updateMeno: '', updatePriezvisko: '' });
+            this.handleLoadClick();
+          })
+          .catch(error => console.error(error));
+      }
+
+      // GET ITEM/S
     handleLoadClick = () => {
         
         this.props.context.spHttpClient.get(`${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('CRUDLIST')/items`, SPHttpClient.configurations.v1)
@@ -128,30 +183,38 @@ export default class ShpApiPAGE extends React.Component<IShpApiPAGEProps, IShpAp
 
     render() {
         return (
-            <div>
+            <div className={styles.shpApiPage}>
                 {/* GET OPERACIA */}
                 <h2>GET</h2>
-                <input type="text" value={this.state.listName} onChange={this.handleInputChange} placeholder='Zadajte názov zoznamu' />
+                {/*<input type="text" value={this.state.listName} onChange={this.handleInputChange} placeholder='Zadajte názov zoznamu' />*/}
                 <button onClick={this.handleLoadClick}>Nacitaj</button>
 
-                {/* GET OPERACIA */}
+                {/* POST OPERACIA */}
                 <h2>POST</h2>
                 <input type="text" value={"Osoba"} readOnly={true} />
                 <input type="text" value={this.state.meno} onChange={this.handleMenoChange} placeholder="Zadajte meno" />
                 <input type="text" value={this.state.priezvisko} onChange={this.handlePriezviskoChange} placeholder="Zadajte priezvisko" />
-        <button onClick={this.handleCreateClick}>Vytvoriť</button>
+                <button onClick={this.handleCreateClick}>Vytvoriť</button>
 
-            <h2>DELETE</h2>
-        <input type="text" value={this.state.deletePriezvisko} onChange={this.handleDeletePriezviskoChange} placeholder="Zadajte priezvisko pre vymazanie" />
-        <button onClick={this.handleDeleteClick}>Vymazať</button>
+                {/* DELETE OPERACIA */}
+                <h2>DELETE</h2>
+                <input type="text" value={this.state.deletePriezvisko} onChange={this.handleDeletePriezviskoChange} placeholder="Zadajte priezvisko pre vymazanie" />
+                <button onClick={this.handleDeleteClick}>Vymazať</button>
+
+                {/* UPDATE OPERACIA */}
+                <h2>UPDATE</h2>
+                <input type="text" value={this.state.updateId} onChange={this.handleUpdateIdChange} placeholder="Zadajte ID položky pre aktualizáciu" />
+                <input type="text" value={this.state.updateMeno} onChange={this.handleUpdateMenoChange} placeholder="Zadajte meno pre aktualizáciu" />
+                <input type="text" value={this.state.updatePriezvisko} onChange={this.handleUpdatePriezviskoChange} placeholder="Zadajte priezvisko pre aktualizáciu" />
+                <button onClick={this.handleUpdateClick}>Aktualizovať</button>
 
 
-         {/* Vypis Zoznamu */}
-            <ul>
-                {this.state.items.map((item: any, index: number) => (
-                    <li key={index}>{item.Title} - {item.Meno} - {item.Priezvisko}</li>
-                ))}
-            </ul>
+                {/* Vypis Zoznamu */}
+                <ul>
+                    {this.state.items.map((item: any, index: number) => (
+                        <li key={index}>{item.ID} - {item.Title} - {item.Meno} {item.Priezvisko}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
